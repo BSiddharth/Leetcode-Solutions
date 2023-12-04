603e44f4e4bf8620259dbce6dfc0c44bdbacf559
Sid <siddharthbisht99@gmail.com>
2023-12-04 13:41:45 +0530

    initial commit

---
+++ two_sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums, target: int):
+        seen = {}
+        for i,x in enumerate(nums):
+            if target - x in seen:
+                return (seen[target - x][0],i)
+            old_record = seen.get(x,[])
+            old_record.append(i)
+            seen[x] = old_record
+
+
+
+        
---
+++ valid_anagram.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        count = {}
+        for x in s:
+            record = count.get(x,0)
+            count[x] = record + 1
+
+        for x in t:
+            if x not in count:
+                return False
+            count[x] -= 1
+            if count[x] < 0:
+                return False
+        
+        for key in count:
+            if count[key] > 0:
+                return False
+
+        return True
