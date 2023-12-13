8cb1ef994436f433ba8d913aca460b44ad8db635
Sid <siddharthbisht99@gmail.com>
2023-12-13 21:32:08 +0530

    updated file genny script

--- _generate_file.py
+++ _generate_file.py
@@ -9,6 +9,8 @@ name_of_file = name_of_file.replace(" ", "_")
 with open(f"{name_of_file}.py", "w") as f:
     f.write(f"# https://leetcode.com/problems/{leetcode_naming}/description/")
     f.write("\n")
-    f.write(f'# git add . && git commit -m "completed {name_of_file}" && git push')
+    f.write(
+        f'# git add . && git commit -m "completed {name_of_file}" && git push && exit'
+    )
 
 print(f"{name_of_file}.py generated")
