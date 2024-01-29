6b5002b0a0f272f3873dbb512f8999ec3159d2c5
Sid <siddharthbisht99@gmail.com>
2024-01-29 17:31:46 +0530

    completed multiply_strings

---
+++ multiply_strings.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/multiply-strings/description/
+# git add . && git commit -m "completed multiply_strings" && git push && exit
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        # stupid question
+        return str(int(num1)*int(num2))
